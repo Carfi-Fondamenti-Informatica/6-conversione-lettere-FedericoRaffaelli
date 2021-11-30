4f65d4f3d269f69cce007ac36b0673a74f3a6990
FedericoRaffaelli <91672486+FedericoRaffaelli@users.noreply.github.com>
2021-11-30 17:27:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    char a;
+    cin >> a;
+    int ris=verifica(a);
+    if(ris){
+        cout << converti(a) << endl;
+    }else{
+        cout << "errore";
+    }
+    return 0;
 }
