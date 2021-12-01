c4cc2040adf48f6d08e32df2f4a548b227f56fc2
FedericoRaffaelli <91672486+FedericoRaffaelli@users.noreply.github.com>
2021-12-01 19:32:55 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,7 +1,7 @@
-#ifndef ESERCITAZIONE__6_LIB_H
-#define ESERCITAZIONE__6_LIB_H
+#ifndef ESERCITAZIONE_6_LIB_H
+#define ESERCITAZIONE_6_LIB_H
 
 bool verifica(char a);
 char converti(char a);
 
-#endif //ESERCITAZIONE__6_LIB_H
+#endif //ESERCITAZIONE_6_LIB_H
